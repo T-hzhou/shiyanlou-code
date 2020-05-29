874666084f662f01b165954a2381a7008d1e23ca
T-hzhou <350727354@qq.com>
2020-05-29 14:56:45 +0800

    frist shangchuan

---
+++ jump7.py
@@ -0,0 +1,27 @@
+#for i in range(1,101):
+	#if i/7==0 or i%7==10 or i/10==7:
+		#continue
+	#else:
+		#print(i)
+
+#for i in range(1,101):
+	#if i/7==0 or i%7==10 or i/10==7:
+		#continue
+	#print(i)
+
+i=0
+while i<100:
+	i=i+1
+	if i/7==0 or i%7==10 or i/10==7:
+		continue
+	else:
+		print(i)
+'''
+i=0
+while i<100:
+	i=i+1
+	if i/7==0 or i%7==10 or i/10==7:
+		continue
+	else:
+		print(i)
+'''
